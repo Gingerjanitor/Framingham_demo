0fae9b671059b755a643196cd64b6654f06dbb1c
Matt Manierre <mmanierr@clarkson.edu>
2023-08-01 07:48:11 -0400

    Finished the module that compiles a scatter plot analysis. A regression model is next.

--- Main_script.py
+++ Main_script.py
@@ -37,14 +37,13 @@ ws=wb.active
 #Show the welcome message
 #Make me a input later
 print(ws["A2"].value)
-print("\n\n\n\n\n") ###Delete me later after input implemented
+print("\n\n\n") ###Delete me later after input implemented
 
 print(ws["C2"].value)
 
 print("\n\n_____________________________________________\n\n")
-print("\n\n")
+
 print(Patients.head(5))
-print("\n\n")
 input("\n\nPress enter to start calculating the Framingham scores")
 
 rawscores=pd.DataFrame()
@@ -59,8 +58,8 @@ Patients['Framingham']=rawscores.sum(axis=1)
 
 print("\n\n_____________________________________________\n\n")
 
+print(f"""{ws["C3"].value}\n\n""")
 
-print(f"""\n\n{ws["C3"].value}\n\n""")
 print(rawscores.head(5))
 
 input("\n\nPress enter to continue")
@@ -69,6 +68,7 @@ print("\n\n_____________________________________________\n\n")
 print("\n\n The rows are then combined and merged into the main dataset\n\n")
 print(Patients.head(5))
 
+input("\n\nPress enter to continue")
 
 ###compute risk tiers
 
@@ -80,6 +80,8 @@ print("\n\n_____________________________________________\n\n")
 print(f"""\n\n{ws["C4"].value}\n\n""")
 
 print(Patients[['Gender', 'Framingham','Risk_pct','Riskcats','Highrisk']].head(5))
+
+input("\n\nPress enter to continue")
 
 #pd.DataFrame.to_csv(Patients,"almost prepared data")
 
@@ -98,9 +100,6 @@ Patients=analyze.gendata(Patients)
 
 print("\n\n_____________________________________________\n\n")
 
-
-print(f"""{ws["C3"].value}\n\n""")
-
 analyze.show_risky(Patients)
 
 print("\n\n_____________________________________________\n\n")
@@ -112,100 +111,41 @@ analyze.crosstab(Patients)
 
 #################
       
-########STEP 2: Show an analysis of heart risk scores and medical expenditures
-###Ask if they want to...
-#   see histograms of both variables (then ask, do you want an interpretation?)
-#   See a scatter plot (then ask, do you want an interpretation?)
-#...Do a t-test
-#...Do a regression.
+###make a couple histograms 
 
+print("\n\n_____________________________________________\n\n")
 
+###press enter to continue###
 
-print("simple analysis of these data.\n  \
-      Do Framingham risk scores correlated with annual medical expenditures? \n \n\
-      NOTE: Please note that all these data are fabricated, any correlations are there because \n \
-      they were designed to be there. My interpretations are technically accurate, but meaningless! \n")
+analyze.makehistogram(Patients)
+        
+print("\n\n_____________________________________________\n\n")
 
-###
-
-### A cross tab and chi square:
-    
-showchi=input(ws['G2'].value)
-while showchi.lower() not in ['y','n']:
-    showchi=str(input("Please enter either Y or N \n"))
-
-if showchi.lower()=="y":
-    print("\n\n_____________________________________________\n\n")  
-    print("Here's a cross tab of risk scores and SNAP values, calculating percentages along columns. \n\n")
-    nopct=pd.crosstab(Patients['Highrisk'],Patients['SNAP'])
-    chi, p, df, expected= chi2_contingency(nopct)
-    tab=(pd.crosstab(Patients['Highrisk'],Patients['SNAP'],normalize='columns').round(4)*100)
-    cramersv=np.sqrt((chi/len(Patients['SNAP']*(len(tab)-1))))
-    print(tab)
-    
-    print(f"\nChi-Square= {chi.round(2)}, p= {p.round(3)}")
-    print(f"Cramer's V= {cramersv.round(3)}")
-
-    
-    explain=str(input("\n Shall I provide an interpretation of these results? Y or N?\n"))
-    while explain.lower() not in ['y','n']:
-        explain=str(input("Please enter either Y or N \n"))
-    
-    if explain.lower()=="y":
-        print(ws['G3'].value)
-
-    
-
-    
-
-###
-##press enter to continue###
-
-print("First, it is important to examine distributions for both variables.")
-pd.join()
+analyze.runscatter(Patients)
+##Mixing in gender
 
 
 
 
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
-f, axes = plt.subplots(1, 2)
-
-sns.histplot(Patients['Framingham'], 
-             bins=15,
-             ax=axes[0])
-axes[0].set_xlabel("Risk Score")
-axes[0].set_title("Framingham Score Distribution")
-axes[0].axvline(x=Patients['Framingham'].mean(),color='red')
-axes[0].axvline(x=Patients['Framingham'].median(),color='blue')
-
-sns.histplot(Patients['Annual_med_costs'],
-             bins=20,
-             ax=axes[1])
-axes[1].set_title("Annual Medical Costs Distribution")
-axes[1].set_xlabel("$ Spent Per Year")
-axes[1].axvline(x=Patients['Annual_med_costs'].mean(),color='red')
-axes[1].axvline(x=Patients['Annual_med_costs'].median(),color='blue')
-
-plt.show()
-
-print(f"Both distributions are relatively normal, but the also pile at fairly high values. \
-      The mean risk score is {Patients['Framingham'].mean().round(2)}, which is much higher than shown in the literature, \
-      which usually finds that scores tend to pile on the low end. This would be of interest to stakeholders, \
-      who might be able to provide clarifications on why scores are so high amongst the sampled group, or it may indicate \
-      a problem in the data handling/collection process- is data being misentered or misclassified during warehousing? \
-      \n \n \
-      ...But for our purposes, let's assume that the data is valid and might be sourced a particularly high risk population \
-      such as attendees at an monthly free clinic for low income and housing insecure persons." )
 
 
-means=Patients['Framingham'].groupby(Patients['Gender']).mean()
-print(f"A simple scatter plot helps to clarify the relationship between the two variables. Here we can see \
-      a clear positive correlation between the two. As Framingham scores increases,  medical \
-      expenditures increase fairly reliably, with a strong Pearson's R of {Patients['Framingham'].corr(Patients['Annual_med_costs']).round(2)}.\n \
-      It is likely that this relationship is not unidirectional- being sick is not easy on your wallet, and stressing about money isn't \
-      going to help that high blood pressure! \n \n \
-          \
+        
+    if showchi.lower()=="y":
+
+
+
+sns.regplot(x = "Risk_pct", y = "Annual_med_costs", data = Patients)
+plt.title("Correlation between Framingham scores \n and medical expenditures", fontsize=18)
+plt.xlabel("10-year risk %", fontsize=15)
+plt.ylabel("Annual medical expenditures", fontsize=15)
+plt.legend(["Female","Male"])
+          
+          
+          
+          
+          
+          
+          
       The coloring of the dots highlights substantial gender differences in the Framingham scores, with men \
       having a mean of {means['M'].round(1)} versus a mean of {means['F'].round(1)} for women \n. At first \
       blush this is confusing, as authors such as Park and Pepine (2015) point out research that highlights that these data underestimate  \
@@ -216,16 +156,6 @@ print(f"A simple scatter plot helps to clarify the relationship between the two 
           
 Print("All this indicates that we really need to control for sex in a final analysis!'")
       
-hue=Patients['Genderdum']
-sns.set_theme()
-#sns.set_style("dark")
-sns.scatterplot(Patients,x='Framingham',y='Annual_med_costs',hue=hue, palette=("rocket"))
-plt.title("Correlation between Framingham scores \n and medical expenditures", fontsize=18)
-plt.xlabel("Framingham score", fontsize=15)
-plt.ylabel("Annual medical expenditures", fontsize=15)
-plt.legend(["Female","Male"])
-
-
 hue=Patients['Genderdum']
 sns.set_theme()
 #sns.set_style("dark")
--- run_analyses.py
+++ run_analyses.py
@@ -9,6 +9,8 @@ import numpy as np
 from openpyxl.workbook import workbook
 from openpyxl import load_workbook
 from scipy.stats import chi2_contingency
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 wb=load_workbook("C:/Users/Matt0/test project/Framingham score tool/Script.xlsx")
@@ -16,9 +18,9 @@ ws=wb.active
 
 def gendata(Patients):
     np.random.seed(29293)
-    Patients['tempcost']=np.random.randint(85,7000, 200)
+    Patients['tempcost']=np.random.randint(85,10000, 250)
     #Impose a correlation between this new variable and Framingham scores
-    Patients['Annual_med_costs']=(Patients['Framingham']+.003*Patients['tempcost'])*100
+    Patients['Annual_med_costs']=(Patients['Framingham']+.002*Patients['tempcost'])*100
     del Patients['tempcost']
 
     #dummy up gender for later
@@ -27,7 +29,7 @@ def gendata(Patients):
     #An indicator of if they are a snap recipient or not
     Patients['SNAP']=np.random.default_rng().choice(["no","yes"],len(Patients['Gender']))
     #doctor it so Patients is correlated with high risk by randomly assigning some of the snap people to be high risk.
-    rand=np.random.randint(0,100,200)
+    rand=np.random.randint(0,100,250)
     Patients.loc[(rand>=35) & (Patients['Highrisk']==">20% risk"), 'SNAP']="yes"
     
     return Patients
@@ -55,6 +57,8 @@ def show_risky(Patients):
         print(header)
         for case in atrisk.itertuples():
             print(f'Patient ID: {case[1]}, risk of {case[2]}%')
+            
+        input("\n\n Press enter when you're finished")
 
 def crosstab(Patients):
 ### A cross tab and chi square:
@@ -76,11 +80,105 @@ def crosstab(Patients):
         print(f"Cramer's V= {cramersv.round(3)}")
 
     
-    explain=str(input("\n Shall I provide an interpretation of these results and next steps? Y or N?\n"))
-    while explain.lower() not in ['y','n']:
-        
-        explain=str(input("Please enter either Y or N \n"))
+        explain=str(input("\n Shall I provide an interpretation of these results and next steps? Enter Y or N?\n"))
+        while explain.lower() not in ['y','n']:
+            
+            explain=str(input("Please enter either Y or N \n"))
     
-    if explain.lower()=="y":
-        print("\n\n_____________________________________________\n\n")
-        print(ws['G3'].value)
+        if explain.lower()=="y":
+            print("\n\n_____________________________________________\n\n")
+            print(ws['G3'].value)
+            input("\n\n Press enter when you're finished")
+
+def makehistogram(Patients):
+        
+    costanalysis=input(ws['I2'].value)
+    while costanalysis.lower() not in ['y','n']:
+        costanalysis=str(input("Please enter either Y or N \n"))
+    
+    if costanalysis.lower()=="y":
+        print("\n\n_____________________________________________\n\n")  
+        print("First, it is important to examine distributions for both variables.\n\n")
+          
+        plt.rcParams["figure.figsize"] = [7.00, 3.50]
+        plt.rcParams["figure.autolayout"] = True
+        f, axes = plt.subplots(1, 2)
+        
+        sns.histplot(data=Patients,
+                     x='Risk_pct',
+                     bins=20,
+                     ax=axes[0])
+        axes[0].set_xlabel("10-year risk %")
+        axes[0].set_title("Framingham Risk Estimate Distribution")
+        axes[0].axvline(x=Patients['Risk_pct'].mean(),color='red')
+        axes[0].axvline(x=Patients['Risk_pct'].median(),color='blue')
+        
+        sns.histplot(Patients['Annual_med_costs'],
+                     bins=20,
+                     ax=axes[1])
+        axes[1].set_title("Annual Medical Costs Distribution")
+        axes[1].set_xlabel("$ Spent Per Year")
+        axes[1].axvline(x=Patients['Annual_med_costs'].mean(),color='red')
+        axes[1].axvline(x=Patients['Annual_med_costs'].median(),color='blue')
+        
+        plt.show()
+        
+        explain=str(input("\n Would you like an explanation of these graphs? Enter Y or N?\n"))
+        while explain.lower() not in ['y','n']:
+            
+            explain=str(input("Please enter either Y or N \n"))
+    
+        if explain.lower()=="y":
+            print("\n\n_____________________________________________\n\n")
+            explanation=ws['I3'].value
+            mean=int(Patients['Annual_med_costs'].mean())
+            std=int(Patients['Annual_med_costs'].std())
+            stdrange=int(std*1.96)
+            explanation=explanation.replace('PATIENTMEAN',f'${mean}')
+            explanation=explanation.replace('PATIENTSTD',f'${std}')
+            explanation=explanation.replace('STDRANGE',f'${stdrange}')
+            input(explanation)
+            
+def runscatter(Patients):
+    
+###Scatter plots
+
+###display the first
+    input(ws['I4'].value)
+    plt.figure(figsize=(8, 5))
+    sns.regplot(x = "Risk_pct", y = "Annual_med_costs", data = Patients)
+    plt.title("Correlation between Framingham scores \n and medical expenditures", fontsize=18)
+    plt.xlabel("10-year risk %", fontsize=15)
+    plt.ylabel("Annual medical expenditures", fontsize=15)
+    plt.show()
+    
+    print("\n_____________________________________________\n")
+    
+    ##display the second
+    input(ws['I5'].value)
+    
+    print("\n_____________________________________________\n")
+    plt.figure(figsize=(8, 5))
+    sns.lmplot(x = "Risk_pct", y = "Annual_med_costs",
+               hue = "Gender", 
+               markers = ["s", "x"],
+               data = Patients,
+               legend=False,
+               facet_kws={'legend_out': True},
+               height=5,
+               aspect=1.5)
+    plt.legend(title="Gender", loc="upper left", bbox_to_anchor=(.75, .25))
+    plt.title("Highlighting sex differences", fontsize=18)
+    plt.xlabel("10-year risk %", fontsize=15)
+    plt.ylabel("Annual medical expenditures", fontsize=15)
+    
+    plt.show()
+    interpretplz=input(ws['I6'].value)
+    while interpretplz.lower() not in ['y','n']:
+        interpretplz=str(input("Please enter either Y or N \n"))
+    
+    if interpretplz=="y":
+        answer=ws['I7'].value
+        answer=answer.replace("CORRCOEF",f"{Patients['Risk_pct'].corr(Patients['Annual_med_costs']).round(2)}")
+        print("\n")
+        input(answer)
